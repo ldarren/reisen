c3953fc92e086fd0ec90cff725ba8fc2748969be
Darren Liew <ldarren@gmail.com>
2023-02-21 01:35:20 +0800

    better env control

--- api/service/index.js
+++ api/service/index.js
@@ -1,11 +1,15 @@
+const psUtil = pico.import('picos-util')
 const pObj = require('pico/obj')
 const base = require('service/base.json')
-const env = require(`service/env.${process.env.NODE_ENV}.json`)
 const routes = require('service/routes.json')
+const env = require('env.json')
 const out = {}
 
 this.load = () => {
-	pObj.extends(out, [base, env, routes])
+	if (env){
+		psUtil.env(pObj.flatten(env))
+	}
+	pObj.replace(base, process.env)
+	pObj.extends(out, [base, routes])
 }
-
 return out
