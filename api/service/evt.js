4ce58971ffaddb5b9fe712c4b39f35ea94c08417
Darren Liew <ldarren@gmail.com>
2023-02-27 01:48:34 +0800

    fixed table schema

--- api/service/evt.js
+++ api/service/evt.js
@@ -1,8 +1,9 @@
 const icalgen = pico.import('ical-generator')
+const pObj = require('pico/obj')
 
 return {
 	async read(output){
-		const res = await this.ps.Event.query().where('s', 1).exec()	
+		const res = await this.ps.event.query().where('s', 1).exec()	
 		output.push(...res.rows)
 		return this.next()
 	},
@@ -11,23 +12,21 @@ return {
 
 		for (let i = 0, l = rows.length, row; i < l; i++){
 			row = rows[i]
-			const startTime = new Date(row.cat)
-			const endTime = new Date(startTime.getTime())
-			endTime.setHours(startTime.getHours()+1)
-			const evt = cal.createEvent({
-				start: startTime,
-				end: endTime,
-				summary: row.name,
-				description: row.desc,
-				location: row.contact.loc,
-				url: row.contact.url || ''
-			})
-			const a = row.contact.alarm
-			evt.createAlarm({
-				triggerAfter: -1 * 60 * 10,
-				description: a.desc,
-				type: a.act
-			})
+			const start = new Date(row.cat)
+			const end = new Date(start.getTime())
+			end.setHours(start.getHours()+1)
+			const evt = cal.createEvent(pObj.extends({
+			}, [row.meta || {}, {
+				id: row.id,
+				start,
+				end,
+				summary: row.summary,
+				description: row.description,
+				location: row.location,
+				sequence: row.sequence,
+				url: row.url
+			}]))
+			row.alarms.forEach(alarm =>  evt.createAlarm(alarm))
 		}
 		data[key] = cal.toString()
 		return this.next()
