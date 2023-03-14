d7d23c96853243f0043aaaaeb2940fbb29bdf49b
Darren Liew <ldarren@gmail.com>
2023-03-15 00:49:36 +0800

    added cors

--- api/mod/util.js
+++ api/mod/util.js
@@ -1,8 +1,23 @@
 const ACCEPT = 'accept'
+const CORS_HEADERS = {
+	'Access-Control-Allow-Origin': 'https://reisen-ui-dev.onrender.com',
+	'Access-Control-Allow-Methods': 'POST, GET, PUT, PATCH, DELETE, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
+	'Access-Control-Max-Age': 86400
+}
 
 module.exports = {
 	setup(host, cfg, rsc, paths){
 		return this
+	},
+
+	handleOption(req, res){
+	console.log(req.method)
+		if ('OPTIONS' === req.method){
+			res.writeHead(204, CORS_HEADERS)
+			res.end()
+		}
+		return this.next()
 	},
 
 	branchByContentType(map, def){
